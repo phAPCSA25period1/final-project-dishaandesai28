455481bf5756bde86c425d2a6f51d2c9df90dd7b
Dishaan Desai <102244332+dishaandesai28@users.noreply.github.com>
2026-04-27 17:06:54 +0000

    created the getValidInput method which handles invalid input from the user

--- src/QuizRunner.java
+++ src/QuizRunner.java
@@ -1,39 +1,56 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.concurrent;
 
 public class QuizRunner
 {
+    /*
+    * Main method that simulates the quiz game
+    */
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Question> questions = new ArrayList<>();
+        boolean playAgain = true;
 
-        showMenu();
-        int subject = scanner.nextInt();
-        scanner.nextLine();
-
-        switch (subject)
+        while (playAgain)
         {
-            case 1:
-                HistoryQuiz hq = new HistoryQuiz();
-                questions = hq.loadQuestions();
-                break;
-            case 2:
-                MathQuiz mq = new MathQuiz();
-                questions = mq.loadQuestions();
-                break;
-            case 3:
-                ChemistryQuiz cq = new ChemistryQuiz();
-                questions = cq.loadQuestions();
-                break;
-            default:
-                System.out.println("Invalid choice!");
+            ArrayList<Question> questions = new ArrayList<>();
+
+            showMenu();
+            int subject = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (subject)
+            {
+                case 1:
+                    HistoryQuiz hq = new HistoryQuiz();
+                    questions = hq.loadQuestions();
+                    break;
+                case 2:
+                    MathQuiz mq = new MathQuiz();
+                    questions = mq.loadQuestions();
+                    break;
+                case 3:
+                    ChemistryQuiz cq = new ChemistryQuiz();
+                    questions = cq.loadQuestions();
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+            runQuiz(questions, scanner);
+
+            System.out.println("Would you like to play again?");
+            System.out.println("1. Yes");
+            System.out.println("2. No");
+
+            int again = getValidInput(scanner, 1, 2);
+            playAgain = (again == 1);
         }
 
-        runQuiz(questions, scanner);
+        System.out.println("\nThanks for playing. Goodbye!");
         scanner.close();
-
     }
 
     /*
@@ -52,8 +69,7 @@ public class QuizRunner
             System.out.println("\nQuestion " + (i+1) + " of " + questions.size());
             questions.get(i).displayQuestion();
             System.out.print("Enter the number of your answer: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // clears the leftover newLine
+            int choice = getValidInput(scanner, 1, 4);
 
             String guess = questions.get(i).getChoice(choice);
 
@@ -78,13 +94,20 @@ public class QuizRunner
     public static void showMenu()
     {
         System.out.println("\n--- Welcome to the Quiz Program ---");
-        System.out.println("Select a subject you wish to study: ");
+        System.out.println("\nSelect a subject you wish to study: ");
+        TimeUnit.SECONDS.sleep(2); 
         System.out.println("1. History");
         System.out.println("2. Math");
         System.out.println("3. Chemistry");
         System.out.println("Enter your choice (number): ");
     }
 
+    /*
+    * Method that displays the user's results at the end of the quiz
+    * @Param int score --> how many questions the user got correct out of 10
+    * @Param int total --> how many questinos the user was quized on
+    * @Param wrong --> an arrayList of the questions the user got incorrect
+    */
     public static void showResults(int score, int total, ArrayList<Question> wrong)
     {
         System.out.println("\n--- Quiz Complete ---");
@@ -116,5 +139,33 @@ public class QuizRunner
                 System.out.println();
             }
         }
+    }
+
+    /*
+    * Method that handles invalid input from the user to promote program functionality
+    * @Param scanner --> input that is given by the user
+    * @Param int min --> lowest option of the number of choices given (ex. type 1 to play again and type 2 to quit; option 1 = min)
+    * @Param int max --> highest option of the number of choices given (ex. type 1 to play again and type 2 to quit; option 2 = max)
+    * @Return input --> returns the int value input
+    */
+    public static int getValidInput(Scanner scanner, int min, int max)
+    {
+        int input = -1;
+
+        while (input < min || input > max)
+        {
+            try {
+                input = Integer.parseInt(scanner.nextLine().trim());
+                if (input < min || input > max)
+                {
+                    System.out.println("Please enter a valid number between " + min + " and " + max + ".");
+                }
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+
+            }
+        }
+        return input;
     }
 }
